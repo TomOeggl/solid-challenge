6f8f15ffc1c080c1d677af6960bfc2f06827fe6e
Iulia Gaina <i.gaina@sportradar.com>
2023-04-06 14:55:05 +0200

    add string output option

--- main.mjs
+++ main.mjs
@@ -3,7 +3,7 @@ import Reader from "./App/Reader.mjs";
 import Parser from "./App/Parser.mjs";
 import Writer from "./App/Writer.mjs";
 
-import { testData } from "./jest-tests/dummyData/dummyData.mjs";
+//import { testData } from "./jest-tests/dummyData/dummyData.mjs";
 
 let reader = new Reader();
 let parser = new Parser();
@@ -14,7 +14,7 @@ let input = "--foo --bar baz secondstring --number 1 --1 12340823051203 --wrong 
 //let input = ["--foo","--bar", "baz", "--number 1 35 --marko 1234 --over " ];
 //let input = [["--foo","--bar"],[ "baz", "--number 1"]];
 
-connsole.log(testData);
+//connsole.log(testData);
 
 let result = new App(input, reader, parser, writer).run();  
 
