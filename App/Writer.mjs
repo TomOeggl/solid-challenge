55dd58a5739690be7c2dd134c51c6da2c6605579
MrDanielHarka <daniel@harka.com>
2023-04-06 15:41:15 +0200

    Add compositeArray functionality

--- App/Writer.mjs
+++ App/Writer.mjs
@@ -1,23 +1,22 @@
-export default class Writer{
-    writePairsToObject(input){
-        let jsonObject = {};
-        input.forEach(pair => {
-            jsonObject[pair[0]] = pair[1];      
-        });
-       
-        return jsonObject;
-    }
+export default class Writer {
+  writePairsToObject(input) {
+    let jsonObject = {};
+    input.forEach(pair => {
+      jsonObject[pair[0]] = pair[1];
+    });
 
-    writePairsToString(input){
-        let string = '';
-        input.forEach(pair => {
-            let prefix = '-';
-            let key = '';
-            let value = pair[1] + '\n';
-            key = prefix + pair[0] + '\n';
-            string += key + value;    
-        });
-        console.log(string);
-    }
+    return jsonObject;
+  }
 
+  writePairsToString(input) {
+    let string = '';
+    input.forEach(pair => {
+      let prefix = '-';
+      let key = '';
+      let value = pair[1] + '\n';
+      key = prefix + pair[0] + ':\n';
+      string += key + value;
+    });
+    console.log(string);
+  }
 }
--- main.mjs
+++ main.mjs
@@ -1,7 +1,7 @@
-import App from "./App/App.mjs";
-import Reader from "./App/Reader.mjs";
-import Parser from "./App/Parser.mjs";
-import Writer from "./App/Writer.mjs";
+import App from './App/App.mjs';
+import Reader from './App/Reader.mjs';
+import Parser from './App/Parser.mjs';
+import Writer from './App/Writer.mjs';
 
 //import { testData } from "./jest-tests/dummyData/dummyData.mjs";
 
@@ -9,14 +9,13 @@ let reader = new Reader();
 let parser = new Parser();
 let writer = new Writer();
 
-
-let input = "--foo --bar baz secondstring --number 1 --1 12340823051203 --wrong abedof";
-//let input = ["--foo","--bar", "baz", "--number 1 35 --marko 1234 --over " ];
+let input =
+  '--foo --bar baz secondstring --number 1 --1 1234 0823051203 --wrong abedof';
+// let input = ['--foo', '--bar', 'baz', '--number 1 35 --marko 1234 --over '];
 //let input = [["--foo","--bar"],[ "baz", "--number 1"]];
 
 //connsole.log(testData);
 
-let result = new App(input, reader, parser, writer).run();  
+let result = new App(input, reader, parser, writer).run();
 
 console.log(result);
-
